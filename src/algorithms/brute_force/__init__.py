e29f7b13871b32b408005b68b710180df7c8ff15
JpCurada <johncurada.02@gmail.com>
2025-05-19 23:08:30 +0800

    refactor: align codes created by contributors to input widgets

---
+++ src/algorithms/brute_force/__init__.py
@@ -0,0 +1,5 @@
+from .bubble_sort import fnBubbleSort as bubble_sort
+from .selection_sort import fnSelectionSort as selection_sort
+from .linear_search import fnLinearSearch as linear_search
+from .knapsack_problem import fnKnapsackBruteForce as knapsack_problem
+from .travelling_salesman import fnTSPBruteForce as travelling_salesman
