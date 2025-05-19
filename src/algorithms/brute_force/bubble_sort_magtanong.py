acca0fb48d748f51328d943b479553a35c631611
Gaboomsz <gabrielandreemagtanong@iskolarngbayan.pup.edu.ph>
2025-05-19 21:53:16 +0800

    feat: added the step list to return in the function

--- src/algorithms/brute_force/bubble_sort_magtanong.py
+++ src/algorithms/brute_force/bubble_sort_magtanong.py
@@ -18,8 +18,12 @@ References:
 #fn_bubble_sort is a function that takes a list as a parameter
 def fnBubbleSort(listInput, ascending=True, isNumbers=True): 
 
+    #listSteps is a variable that stores the steps of the sorting process
+    listSteps = []
+
     #intRange is the length of the list
     intSize = len(listInput) 
+    
 
     #check if the list is of numbers or letters
     if not isNumbers:
@@ -27,26 +31,30 @@ def fnBubbleSort(listInput, ascending=True, isNumbers=True):
             #convert the elements to string
             listInput[i] = str(listInput[i])
 
-    #Outer loop to iterate through the list
-    for i in range(intSize): 
+    if ascending:
+        #Outer loop to iterate through the list
+        for i in range(intSize): 
 
-        # Inner loop to compare adjacent elements
-        for j in range(0, intSize - i - 1):
+            # Inner loop to compare adjacent elements
+            for j in range(0, intSize - i - 1):
 
-            # Compare if adjacent elements are in the correct order
-            if listInput[j] > listInput[j+1]:
+                # Compare if adjacent elements are in the correct order
+                if listInput[j] > listInput[j+1]:
 
-                # Swap if they are not in the correct order 
-                listInput[j], listInput[j+1] = listInput[j+1], listInput[j]
-                
-                # Repeat until the list is sorted
+                    # Swap if they are not in the correct order 
+                    listInput[j], listInput[j+1] = listInput[j+1], listInput[j]
+                    
+                    # Store the steps in the listSteps variable
+                    listSteps.append(listInput.copy())  
+                    
+                    # Repeat until the list is sorted
+    else:
+        for i in range(intSize):
+            for j in range(0, intSize - i - 1):
+                if listInput[j] < listInput[j + 1]:
+                    listInput[j], listInput[j + 1] = listInput[j + 1], listInput[j]
+                    listSteps.append(listInput.copy())
     
-    #reverse the list if not in ascending order
-    if not ascending:
-        for i in range(intSize // 2):
-            # swap elements from the starting index and the ending index
-            listInput[i], listInput[intSize - 1 - i] = listInput[intSize - 1 - i], listInput[i]
-
-    # Return the sorted list
-    return listInput 
+    #return the sorted list and the steps list
+    return listInput, listSteps
 
